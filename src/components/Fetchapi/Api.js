fdb8124d2650244e7e331df7db63b47ae9479484
Nikita Mishra <86710416+NikitaMishraaa@users.noreply.github.com>
2021-11-23 19:59:05 +0530

    update Api

--- src/components/Fetchapi/Api.js
+++ src/components/Fetchapi/Api.js
@@ -1,15 +1,23 @@
+//<---------------This is only example of fetching data from Api using Mockapi website--------------->
+
 const Api = [
 
     {
-        "id": "2",
-        "firstName": "nikita",
-        "lastName": "tiwari"
+        "id":"2",
+        "firstName":"Nikita",
+        "lastName":"Mishra"
     },
 
     {
         "id": "3",
         "firstName": "suman",
         "lastName": "tddd"
+    },
+
+    {
+        "id":"4",
+        "firstName":"gopal",
+        "lastName":"dubey"
     }
 ]
 
